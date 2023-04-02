3d153587a097d07d02c9b4eb18523dad8cffb0fa
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-02 23:40:56 +0530

    Time: 0 ms (100.00%), Space: 45.6 MB (72.94%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,12 +1,59 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
         
-        int left = searchLeft(nums, target);
-        int right = searchRight(nums, target);
-        
-        return new int[]{left, right};
+        //3rd Template
+        //int left = searchLeft(nums, target);
+        //int right = searchRight(nums, target);
+        //return new int[]{left, right};
+
+        //4th Template
+        int left = searchLeftLoop(nums, target);
+        int right = searchRightLoop(nums, target);
+        return new int[] {left, right};
+
     }
     
+    //4th template Start
+    private int searchLeftLoop (int[] nums, int target){
+        //Starting lo at -1 helps with edge case when we couldn't make a single jump
+        //if we start with 0 then still we can do it, but then we will have to add one extra
+        // check before returning
+        int lo = -1, n = nums.length;
+        for(int jump = n/2; jump >= 1; jump/=2){
+            while((lo + jump < n) && isNumberSmallerThanTarget(nums, lo+jump, target)){
+                lo += jump;
+            }
+        }
+        if(lo +1 >= n)
+            return -1;
+        return nums[lo+1] == target ? lo+1 : -1;
+    }
+
+    //TTTTTT'F'FFFF
+    private boolean isNumberSmallerThanTarget(int[] nums, int x, int target){
+        return nums[x] < target;
+    }
+
+
+    private int searchRightLoop(int[] nums, int target){
+        int lo = 0, n = nums.length;
+        for(int jump = n/2; jump >= 1; jump/=2){
+            while((lo + jump < n) && isNumberSmallerEqualThanTarget(nums, lo+jump, target)){
+                lo += jump;
+            }
+        }
+        if(lo >= n)
+            return -1;
+        return nums[lo] == target ? lo : -1;
+    }
+
+    //TTTTT'T'FFFFF
+    private boolean isNumberSmallerEqualThanTarget(int[] nums, int x, int target){
+        return nums[x] <= target;
+    }
+    //4th template End
+    
+    //3rd Template Start
     private int searchLeft(int[] nums, int target){
         if(nums == null || nums.length == 0){
             return -1;
@@ -43,5 +90,5 @@ class Solution {
         
         return nums[lo]==target? lo : -1;
     }
-    
+    //3rd Template End
 }
