8aca4be75515a683524cf1149f2a502cc2aedd36
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-02 09:38:47 +0530

    Time: 74 ms (67.77%), Space: 63.2 MB (20.66%) - LeetHub

---
+++ 2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int[] result = new int[spells.length];
+        
+        Arrays.sort(potions);
+        
+        for(int i = 0; i < spells.length; i++){
+            int spell = spells[i];
+            int count = 0;
+            int foundIndex = findIndex(spell, potions, success);
+            count = potions.length-foundIndex;
+            
+            result[i] = count;
+        }
+        
+        return result;
+    }
+    
+    public int findIndex(int spell, int[] potions, long success){
+        int lo = 0, hi = potions.length;
+        while(lo+1 < hi){
+            int mid = lo + (hi-lo)/2;
+            if((long)potions[mid]*spell >= success){
+                hi = mid;
+            }else{
+                lo = mid;
+            }
+        }
+        
+        return (long)potions[lo]*spell>= success ? lo : hi;
+    }
+}
