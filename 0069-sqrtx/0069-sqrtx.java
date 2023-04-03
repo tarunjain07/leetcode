f1cfa9944edff99f98645f48604510764a905007
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-03 17:21:54 +0530

    Time: 1 ms (100.00%), Space: 39.5 MB (76.28%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public int mySqrt(int x) {
         if(x == 0) return 0;
         int lo =1, hi = x;
@@ -12,8 +13,54 @@ class Solution {
         }
         return lo;
     }
-    
+
+    // Is mid ^ 2 <= x
+    //TT'T'FFFFFF
     private boolean ok(int x, int mid){
         return mid <= x/mid;
     }
+    
+    
+    // Sqrt with float result - start
+    public double mySqrt_6Decimal(int x){
+        if(x == 0) return 0;
+        double lo = 1, hi = x, epsilon = 1e-6;
+        while(lo + epsilon < hi){
+            double mid = lo + (hi-lo)/2;
+            if(ok2(x, mid)){
+                lo = mid;
+            }else{
+                hi = mid;
+            }
+        }
+        
+        return lo;
+    }
+    
+    private boolean ok2(int x, double mid){
+        return mid <= x/mid;
+    }
+    // Sqrt with float result - end
+    
+    
+    // Sqrt with float result using for loop- start
+    public double sqrt2(int x){
+        if(x==0) return 0;
+        double lo = 1, hi = x;
+        for(int i = 0; i < 80; i++){  
+            //why 80? ->  to guess 10 we need 7 guess => 2^7 = 128,
+            //double is 8bytes i.e 64 bit so 80 just a conservative number
+            double mid = lo + (hi - lo)/2;
+            if(ok2(x, mid)){
+                lo = mid;
+            }else{
+                hi = mid;
+            }
+        }
+        return lo;
+        
+    }
+    // Sqrt with float result using for loop- end
+
+    
 }
