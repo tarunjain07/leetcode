64a955027c41b1796dd523c46c153c19369e6ca9
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-04 09:22:19 +0530

    Time: 12 ms (81.99%), Space: 43.3 MB (45.13%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -31,4 +31,22 @@ class Solution {
         return count;
     }
     //little smarter way end
+    
+    
+    // using Bit manipluation - TODO - couldn't understand it rightnow
+    // What's the use of 1<<shift?
+     public int partitionString_BitManipluation(String s) {
+        int ans = 1;
+        int mask = 0;
+        for (char ch : s.toCharArray()) {
+            int shift = ch - 'a';
+            if ((mask & (1 << shift)) > 0) {
+                ans++;
+                mask = 0;
+            }
+            mask |= 1 << shift;
+        }
+        return ans;
+    }
+    
 }
