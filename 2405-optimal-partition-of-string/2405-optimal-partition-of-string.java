4cc206af4c2ad18f87d6b737889fa69eee3f1640
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-04 09:18:40 +0530

    Time: 11 ms (85.92%), Space: 42.6 MB (96.17%) - LeetHub

--- 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
+++ 2405-optimal-partition-of-string/2405-optimal-partition-of-string.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int partitionString(String s) {
+    public int partitionString_UsingMap(String s) {
         Set<Character> charSet = new HashSet<>();
         int count = 0;
         for(char c:s.toCharArray()){
@@ -13,4 +13,22 @@ class Solution {
         return (charSet.isEmpty()) ? count : count+1;
         
     }
+    
+    //little smarter way
+    public int partitionString(String s) {
+        int[] lastSeen = new int[26];
+        Arrays.fill(lastSeen, -1);
+        
+        int count = 1, subStringStart = 0;
+        for(int i = 0; i < s.length(); i++){
+            if(lastSeen[s.charAt(i) - 'a'] >= subStringStart){
+                count++;
+                subStringStart = i;
+            }
+            lastSeen[s.charAt(i) -'a'] = i;
+        }
+        
+        return count;
+    }
+    //little smarter way end
 }
