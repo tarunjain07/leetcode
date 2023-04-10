14244b886616eed1d6338376b2bfd6bccf56c620
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-10 14:17:26 +0530

    Time: 2 ms (79.07%), Space: 40.3 MB (74.90%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -6,23 +6,23 @@ class Solution {
         for(char c: s.toCharArray()){
             if(c == '(' || c == '[' || c == '{'){
                 stack.push(c);
-            }else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                
-                char popChar = stack.pop();
-                
-                if(c == ')'){
-                    if(popChar != '(') return false;
-                }
-                else if(c == ']'){
-                   if(popChar != '[') return false;
-                }
-                else if(c == '}'){
-                    if(popChar != '{') return false;
-                }
-                
+                continue;
+            }
+            
+            if(stack.isEmpty()){
+                return false;
+            }
+
+            char popChar = stack.pop();
+
+            if(c == ')'){
+                if(popChar != '(') return false;
+            }
+            else if(c == ']'){
+               if(popChar != '[') return false;
+            }
+            else if(c == '}'){
+                if(popChar != '{') return false;
             }
         }
         
