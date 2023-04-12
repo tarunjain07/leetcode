4894cc8984fbe5021dd4467af1c965da7d759f21
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-04-12 15:08:23 +0530

    Time: 3 ms (98.32%), Space: 42.5 MB (63.19%) - LeetHub

---
+++ 0071-simplify-path/0071-simplify-path.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String simplifyPath(String path) {
+        String[] splittedPaths = path.split("/");
+        
+        Deque<String> stack = new ArrayDeque<>();
+        
+        for(String splitPathVal: splittedPaths){
+            if(splitPathVal.equals("") || splitPathVal.equals(".")){
+                continue;
+            }
+            
+            else if(splitPathVal.equals("..")){
+                if(stack.isEmpty()){
+                    continue;
+                }else{
+                    stack.removeLast();
+                }
+            }
+            else{
+                stack.addLast(splitPathVal);
+            }
+            
+       }
+        
+        StringBuilder result  = new StringBuilder();
+        
+        while(!stack.isEmpty()){
+            result.append("/");
+            String val = stack.removeFirst();
+            result.append(val);
+        }
+        
+        return result.toString().isEmpty()?"/" :  result.toString();
+            
+        
+    }
+}
