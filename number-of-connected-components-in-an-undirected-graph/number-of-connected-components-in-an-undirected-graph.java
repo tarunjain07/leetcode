fd17dfc6f235335feb523e092829a2ea29840484
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-05-01 17:50:02 +0530

    Time: 1 ms, Memory: 42.6 MB - LeetHub

---
+++ number-of-connected-components-in-an-undirected-graph/number-of-connected-components-in-an-undirected-graph.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int countComponents(int n, int[][] edges) {
+        UnionFind uf = new UnionFind(n);
+        for(int[] edge: edges){
+            uf.union(edge[0], edge[1]);
+        }
+        
+        return uf.getCount();
+    }
+    
+    class UnionFind {
+        private int[] root;
+        private int[] rank;
+        private int count;
+
+        UnionFind(int size) {
+            root = new int[size];
+            rank = new int[size];
+            count = size;
+            for (int i = 0; i < size; i++) {
+                root[i] = i;
+                rank[i] = 1;
+            }
+        }
+
+        int find(int x) {
+            if (x == root[x]) {
+                return x;
+            }
+            return root[x] = find(root[x]);
+        }
+
+        void union(int x, int y) {
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX != rootY) {
+                if (rank[rootX] > rank[rootY]) {
+                    root[rootY] = rootX;
+                } else if (rank[rootX] < rank[rootY]) {
+                    root[rootX] = rootY;
+                } else {
+                    root[rootY] = rootX;
+                    rank[rootX] += 1;
+                }
+                count--;
+            }
+        }
+
+        int getCount() {
+            return count;
+        }
+    }
+}
