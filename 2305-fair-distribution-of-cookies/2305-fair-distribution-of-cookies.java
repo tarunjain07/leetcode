70174f0b866c9cf9201d2b225b19552ebcb9b5d2
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-07-02 11:37:27 +0530

    Time: 828 ms (5.15%), Space: 43.7 MB (5.15%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.java
@@ -1,41 +1,31 @@
 class Solution {
-    private int dfs(int i, int[] distribute, int[] cookies, int k, int zeroCount) {
-        // If there are not enough cookies remaining, return Integer.MAX_VALUE
-        // as it leads to an invalid distribution.
-        if (cookies.length - i < zeroCount) {
-            return Integer.MAX_VALUE;   
-        }
-
-        // After distributing all cookies, return the unfairness of this
-        // distribution.
-        if (i == cookies.length) {
-            int unfairness = Integer.MIN_VALUE;
-            for (int value : distribute) {
-                unfairness = Math.max(unfairness, value);
-            }
-            return unfairness;
-        }
-        
-        // Try to distribute the i-th cookie to each child, and update answer
-        // as the minimum unfairness in these distributions.
-        int answer = Integer.MAX_VALUE;
-        for (int j = 0; j < k; ++j) {
-            zeroCount -= distribute[j] == 0 ? 1 : 0;
-            distribute[j] += cookies[i];
-            
-            // Recursively distribute the next cookie.
-            answer = Math.min(answer, dfs(i + 1, distribute, cookies, k, zeroCount));
-            
-            distribute[j] -= cookies[i];
-            zeroCount += distribute[j] == 0 ? 1 : 0;
-        }
-        
-        return answer;
-    }
+    int n;
+    int result = Integer.MAX_VALUE;
     
     public int distributeCookies(int[] cookies, int k) {
-        int[] distribute = new int[k];
         
-        return dfs(0, distribute, cookies, k, k);
+        n = cookies.length;
+        int[] children = new int[k];
+        
+        solve(0, cookies, children, k);
+        return result;
+    }
+    
+    void solve(int idx, int[] cookies, int[] children, int k){
+        
+        if(idx>= n){
+            int unfairness = Arrays.stream(children).max().getAsInt();
+            result = Math.min(result, unfairness);
+            return;
+        }
+        int cookie = cookies[idx];
+        for(int c = 0; c < k; c++){
+            children[c] += cookie;
+            if(children[0] == 0){
+                break;
+            }
+            solve(idx+1, cookies, children, k);
+            children[c] -= cookie;
+        }
     }
 }
