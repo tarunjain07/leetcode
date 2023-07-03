6da7151be638e44414c34ed57dc9f681f320273e
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-07-03 15:34:56 +0530

    Time: 3 ms (65.16%), Space: 41.6 MB (57.51%) - LeetHub

---
+++ 0859-buddy-strings/0859-buddy-strings.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean buddyStrings(String s, String goal) {
+        if(s.length() != goal.length()){
+            return false;
+        }
+        
+        if(s.equals(goal)){
+            return checkFreq(s);
+        }
+        
+        List<Integer> unequalIndicies = new ArrayList<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) != goal.charAt(i)){
+                unequalIndicies.add(i);
+            }
+        }
+        
+        if(unequalIndicies.size() !=2){
+            return false;
+        }
+        
+        char[] sCharArray = s.toCharArray();
+        char temp = sCharArray[unequalIndicies.get(0)];
+        sCharArray[unequalIndicies.get(0)] = sCharArray[unequalIndicies.get(1)];
+        sCharArray[unequalIndicies.get(1)] = temp;
+        
+        if(new String(sCharArray).equals(goal)){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    boolean checkFreq(String s){
+        int[] arr = new int[26];
+        for(char c: s.toCharArray()){
+            arr[c-'a']++;
+            if(arr[c - 'a'] > 1){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    
+}
