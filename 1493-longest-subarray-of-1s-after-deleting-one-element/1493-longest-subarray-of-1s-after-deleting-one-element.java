deeb7ba542bd23ba4cd4ee551e64b2f7cc717169
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-07-15 10:39:52 +0530

    Time: 2 ms (84.67%), Space: 55.9 MB (5.45%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,6 +1,26 @@
 class Solution {
     
     public int longestSubarray(int[] nums) {
+        int lastZeroIndex = -1;
+        int result = 0;
+        int i = 0, j = 0;
+        while(j < nums.length){
+            if(nums[j] == 0){
+                i = lastZeroIndex+1;
+                lastZeroIndex = j;
+            }
+            
+            result = Math.max(result, j-i);
+            j++;
+        }
+        
+        return result;
+    }
+    
+    
+    // Time complexity - every element is being touched max 2 times
+    // O(n+n) ~ O(n)
+    public int longestSubarray_slidingWindowBasic(int[] nums) {
         
         int zeroCount = 0, i = 0, result = 0, n = nums.length;
         
