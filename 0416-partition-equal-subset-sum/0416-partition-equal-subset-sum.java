95e457e20cff3302da5f5d59e1dfd5a3d9cf271f
Tarun Jain <tarunjain07@users.noreply.github.com>
2023-08-02 22:28:24 +0530

    Time: 41 ms (76.67%), Space: 46.4 MB (65.28%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,7 +1,40 @@
 class Solution {
-    Boolean[][] t;
     
     public boolean canPartition(int[] nums) {
+        int totalSum = Arrays.stream(nums).sum();
+        
+        if(totalSum%2 != 0)
+            return false;
+        
+        int n = nums.length;
+        int sum = totalSum/2;
+        
+        boolean[][] dp = new boolean[n+1][sum+1];
+    
+        for(int row = 0; row <= n; row++){
+            dp[row][0] = true;
+        }
+       
+
+        
+        
+        for(int row = 1; row <= n; row++){  // elements from array
+            for(int col = 1; col <= sum; col++){ // sum value
+                int val = nums[row-1];
+                if(val > col){
+                    dp[row][col] = dp[row-1][col];
+                }else{
+                    dp[row][col] = dp[row-1][col] || dp[row-1][col-val];
+                }
+            }
+            
+        }
+        
+        return dp[n][sum];
+    
+    }
+            
+    public boolean canPartition_memo(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         
         if(sum%2 != 0)
@@ -9,14 +42,14 @@ class Solution {
         
         int n = nums.length;
 
-        this.t = new Boolean[n+1][sum+1];
+        Boolean[][] t = new Boolean[n+1][sum+1];
 
-        boolean result = helper(n, nums, sum/2);
+        boolean result = helper(n, nums, sum/2, t);
         
         return result;
     }
     
-    private boolean helper(int n, int[] nums, int sum){
+    private boolean helper(int n, int[] nums, int sum, Boolean[][] t){
         if(sum ==  0)
             return true;
         if(n == 0)
@@ -27,10 +60,10 @@ class Solution {
         }
         
         if(nums[n-1] <= sum){
-            return t[n][sum] =  helper(n-1, nums, sum-nums[n-1]) || helper(n-1, nums, sum);
+            return t[n][sum] =  helper(n-1, nums, sum-nums[n-1], t) || helper(n-1, nums, sum, t);
             
         }else{
-            return t[n][sum] = helper(n-1, nums, sum);
+            return t[n][sum] = helper(n-1, nums, sum, t);
         }
     }
 }
